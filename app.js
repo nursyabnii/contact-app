3521d33aed82e6dc2e05bea9db67a84d0f1c3c69
nursyabnii <baniharis12@gmail.com>
2024-02-15 10:26:21 +0700

    Update Contact App

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
+// Mengambila argument dari command line
+
+// console.log(process);
+
+// const command = process.argv[2];
+// if (command === 'add') {
+    
+// } else if (command === 'remove') {
+
+// }  else if (command === 'list'){
+
+// }
+const yargs = require('yargs');
 const contacts = require('./contacts');
+// console.log(yargs.argv);
 
-const main = async () => {
-    const nama = await contacts.tulisPertanyaan('Masukkan nama kamu: ');
-    const email = await contacts.tulisPertanyaan('Masukkan email kamu: ');
-    const noHP= await contacts.tulisPertanyaan('Masukkan no hp kamu: ');
+// yargs.command(
+//     'add', 
+//     'Menambahkan contact baru', 
+//     () => {}, 
+//     (argv) => {
+//         console.log(argv.nama);
+//     }
+// );
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+        nama: {
+            describe: "Nama Lengkap",
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: "Email",
+            demandOption: false,
+            type: 'string',
+        },
+        noHP: {
+            describe: "Nomor Handphone",
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    },
+});
 
-    contacts.simpanContact(nama, email, noHP);
-};
+yargs.parse();
 
-main();
+// const contacts = require('./contacts');
+
+// const main = async () => {
+//     const nama = await contacts.tulisPertanyaan('Masukkan nama kamu: ');
+//     const email = await contacts.tulisPertanyaan('Masukkan email kamu: ');
+//     const noHP= await contacts.tulisPertanyaan('Masukkan no hp kamu: ');
+
+//     contacts.simpanContact(nama, email, noHP);
+// };
+
+// main();
--- contacts.js
+++ contacts.js
@@ -1,10 +1,6 @@
 const fs = require ('fs');
-const Readline = require('readline');
-
-const rl = Readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const chalk = require ('chalk');
+const validator = require ('validator');
 
 // membuat folder data
 const dirPath = './data';
@@ -18,25 +14,40 @@ if(!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (nama) => {
-            resolve(nama)
-        });
-    });
-};
-
 const simpanContact = (nama, email, noHP) => {
-    const contact = { nama, email, noHP };
-        const file = fs.readFileSync('data/contacts.json', 'utf-8');
-        const contacts = JSON.parse(file);
+        const contact = { nama, email, noHP };
+        const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+        const contacts = JSON.parse(fileBuffer);
+
+        // cek duplikat
+        const duplikat = contacts.find((contact) => contact.nama === nama);
+        if (duplikat) {
+            console.log(
+                chalk.red.inverse.bold('Contact sudah terdafatar gunakan nama lain')
+            );
+            return false;
+        }
+
+        // cek email
+        if (email) {
+            if(!validator.isEmail(email)) {
+                console.log(chalk.red.inverse.bold('Email tidak Valid!'));
+                return false;
+            }
+        }
+
+        if (email) {
+            if(!validator.isMobilePhone(noHP, 'id-ID')) {
+                console.log(chalk.red.inverse.bold('Nomor HP tidak Valid!'));
+                return false;
+            }
+        }
 
         contacts.push(contact);
 
         fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-        console.log('Terimakasih Telah Mengisi Data Kamu');
-        rl.close();
+        console.log(chalk.green.inverse.bold('Terimakasih Telah Mengisi Data Kamu'));
 };
 
-module.exports = { tulisPertanyaan, simpanContact }
+module.exports = { simpanContact }
